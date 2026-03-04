e760c92fb23a1cc61eca9aad86638a041bfeec7e
ajeet0805 <ajeet10lodhi@gmail.com>
2026-03-04 13:59:10 +0530

    Add files via upload

--- React/src/components/taskTblHeader.tsx
+++ React/src/components/taskTblHeader.tsx
@@ -2,11 +2,11 @@ import { Row } from "react-bootstrap"
 const TaskTblHeader=()=>{
     return(
          <Row className='p-2 mb-2 bg-dark text-white' style={{ marginTop: '10px' }}>
-                <div className='col-4'>ID</div>
+                <div className='col-2'>ID</div>
                 <div className='col-2'>Title</div>
-                <div className='col-3'>Description</div>
+                <div className='col-4'>Description</div>
                 <div className='col-2'> Priority</div>
-                <div className='col-1'>Status</div>               
+                <div className='col-2'>Status</div>               
                 
             </Row>
     )
--- React/src/components/taskTblRow.tsx
+++ React/src/components/taskTblRow.tsx
@@ -1,13 +1,20 @@
 import { Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 import type { Task } from "../interfaces/task";
-const TaskTblRow = (props: Task) => {
-    const { id, title, description, priority, status } = props;
+
+type TaskTblRowProps = Task & {
+  index: number;
+  onRowClick?: (taskId: string) => void;
+};
+
+const TaskTblRow = (props: TaskTblRowProps) => {
+    const { id, title, description, priority, status,onRowClick } = props;
     return (<Row className="row  border-bottom border-dark padding-row p-2 row">
-        <div className='col-4'>{id}</div>
+        <div className='col-2'>{id.substring(0,8)}</div>
         <div className='col-2'>{title}</div>
-        <div className='col-3'>{description}</div>
+        <div className='col-4'>{description}</div>
         <div className='col-2'>{priority} </div>
-        <div className='col-1'> {status}</div>
+        <div className='col-2'> {status} <Button onClick={() => onRowClick?.(id)} className="ms-1">Delete</Button></div>
     </Row>)
 }
 
